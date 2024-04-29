9adb5441ddfda60194158175b7da9f1094a283c5
Matt Deuschle <mdeuschle@gmail.com>
2024-04-28 17:12:48 -0700

    hello world

---
+++ Hi/Hi/HiApp.swift
@@ -0,0 +1,17 @@
+//
+//  HiApp.swift
+//  Hi
+//
+//  Created by Matt Deuschle on 4/28/24.
+//
+
+import SwiftUI
+
+@main
+struct HiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
